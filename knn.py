2d29ac235a3a7a9f190fd3b00c89c6b15e6f6550
JeonSuHwan <jsuhwan34@gmail.com>
2020-05-29 23:42:50 +0900

    knn algorithm: add dataset

---
+++ knn.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Make dataset with page 86
+df = pd.DataFrame({'Height(cm)':[158, 158, 158, 160, 160, 163, 163, 160, 163, 165, 165, 165, 168, 168, 168, 170, 170, 170], 
+                        'Weight(kg)':[58, 59, 63, 59, 60, 60, 61, 64, 64, 61, 62, 65, 62, 63, 66, 63, 64, 68], 
+                        'T_shirt_size':['M', 'M', 'M', 'M', 'M', 'M', 'M', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L']})
+
+
+
+
